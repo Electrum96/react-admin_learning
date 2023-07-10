b0c0c99b962ab40154f5999eba22c20c02ceae06
For_tuna <kachalina.kate96@mail.ru>
2023-07-10 18:45:38 +0300

    List for desktop and mobile

--- src/users.tsx
+++ src/users.tsx
@@ -1,13 +1,31 @@
-import { Datagrid, EmailField, List, TextField } from 'react-admin';
-import { SimpleList } from 'react-admin';
+import {Datagrid, EmailField, List, TextField} from 'react-admin';
+import {SimpleList} from 'react-admin';
+import {useMediaQuery} from '@mui/material'; // отслеживает совпадения с медиа-запросом
 
-export const UserList = () => (
-    <List>
-      <SimpleList
-        primaryText={(record) => record.name}
-        secondaryText={(record) => record.username}
-        tertiaryText={(record)  => record.email}
-      />
-    </List>
-  );
-//List считывает параметры запроса из url, вызывает API, создаёт набор обратных вызовов
+export const UserList = () => {
+    const isSmall = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+    return (
+        <List>
+            
+            {isSmall ? (<SimpleList
+                    primaryText={(record) => record.name} 
+                    secondaryText={(record) => record.username}
+                    tertiaryText={(record) => record.email}/>
+                    ) : (
+                    <Datagrid rowClick="edit">
+                        <TextField source="id"/>
+                        <TextField source="name"/>
+                        <TextField source="username"/>
+                        <EmailField source="email"/>
+                        <TextField source="address.street"/>
+                        <TextField source="phone"/>
+                        <TextField source="website"/>
+                        <TextField source="company.name"/>
+                    </Datagrid>
+
+                )} 
+                
+        </List>
+    );
+
+}; //внутри условие, в зависимости от значения isSmall рендерится компонент для десктопной или мобильной версии
