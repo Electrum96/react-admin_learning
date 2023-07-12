aeda8e05dd0fd709bafb3e584df5e76e6d036fb0
For_tuna <kachalina.kate96@mail.ru>
2023-07-12 14:08:40 +0300

    12.07.2023

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';//комноненты react-admin для совершения CRUD
 import { dataProvider } from './dataProvider';//поставщик данных
 import { UserList } from './users'; //компонент списка, который можно настроить 
+import { PostList } from './posts';// компонент с постами
 
 export const App = () => (
   <Admin dataProvider={dataProvider}>
-   <Resource name="users" list={UserList} /> 
+   <Resource name="posts" list={PostList} />
+   <Resource name="users" list={UserList} recordRepresentation="name"/> 
+   
   </Admin>
 );
--- src/users.tsx
+++ src/users.tsx
@@ -1,29 +1,17 @@
-import {Datagrid, EmailField, List, TextField} from 'react-admin';
-import {SimpleList} from 'react-admin';
-import {useMediaQuery} from '@mui/material'; // отслеживает совпадения с медиа-запросом
+import {Datagrid, EmailField, List, UrlField,TextField} from 'react-admin';
+import MyUrlField from './MyUrlField';
 
 export const UserList = () => {
-    const isSmall = useMediaQuery((theme) => theme.breakpoints.down("sm"));
     return (
         <List>
-            
-            {isSmall ? (<SimpleList
-                    primaryText={(record) => record.name} 
-                    secondaryText={(record) => record.username}
-                    tertiaryText={(record) => record.email}/>
-                    ) : (
-                    <Datagrid rowClick="edit">
-                        <TextField source="id"/>
-                        <TextField source="name"/>
-                        <TextField source="username"/>
-                        <EmailField source="email"/>
-                        <TextField source="address.street"/>
-                        <TextField source="phone"/>
-                        <TextField source="website"/>
-                        <TextField source="company.name"/>
-                    </Datagrid>
-
-                )} 
+            <Datagrid rowClick="edit">
+            <TextField source="id" />
+            <TextField source="username" />
+            <EmailField source="email" />
+            <TextField source="phone" />
+            <MyUrlField source="website"/>
+            <TextField source="company.name" />
+        </Datagrid>
                 
         </List>
     );
