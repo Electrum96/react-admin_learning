656522831c7341d9446511a3c741ac6a152dcd0f
For_tuna <kachalina.kate96@mail.ru>
2023-07-19 19:50:57 +0300

    form test

--- cypress/e2e/test1.cy.ts
+++ cypress/e2e/test1.cy.ts
@@ -2,4 +2,7 @@
 
 it('Google Search', function(){
     cy.visit('http://localhost:5173')
+    cy.get('#username').type('randomEmail@mail.ru')
+    cy.get('#password').type('password')
+    cy.get('.MuiButtonBase-root').click()
 })
