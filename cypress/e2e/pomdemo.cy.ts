61ca97b62c16f4eb3999f8b8ddd6bfa4d00f4cb7
For_tuna <kachalina.kate96@mail.ru>
2023-07-20 17:09:05 +0300

    group tests

--- cypress/e2e/pomdemo.cy.ts
+++ cypress/e2e/pomdemo.cy.ts
@@ -1,18 +1,33 @@
 import { LoginPage } from "./pages/login"
 
 const loginPage = new LoginPage()
-it('POM Demo', function() {
 
+beforeEach(function() {
     cy.visit('http://localhost:5173/')
 
+})
+
+describe('All tests', () => {
+
+it('Test1', function() {
     loginPage.enterUserName('email')
     loginPage.enterPassword('password')
-    loginPage.clickLogin()
+    loginPage.clickLogin()  
+    cy.get('[href="#/users"]').click()
 
-    // cy.get('#username').type('usualemail@mail.ru')
-    // cy.get('#password').type('password')
-    // cy.get('.MuiButtonBase-root').click()
+})
 
-   
+it('Test2', function() {
+    loginPage.enterUserName('email')
+    loginPage.enterPassword('password')
+    loginPage.clickLogin()  
+    cy.get('[href="#/posts"]').click()
 
+})
+})
+
+it('Test3', function() {
+    loginPage.enterUserName('email')
+    loginPage.enterPassword('password')
+    loginPage.clickLogin()  
 })
